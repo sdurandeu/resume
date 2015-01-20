d0c01e633eb89289a9899a814aade24fdd09159e
sdurandeu <sebastiandurandeu@gmail.com>
2015-01-19 23:01:23 -0300

    Added code to calculate age

--- js/resume.js
+++ js/resume.js
@@ -1,7 +1,24 @@
+function getAge(dateString) {
+  // logic from http://stackoverflow.com/questions/4060004/calculate-age-in-javascript (not perfect)
+  var today = new Date();
+  var birthDate = new Date(dateString);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var monthDiff = today.getMonth() - birthDate.getMonth();
+
+  if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+  };
+
+  return age;
+};
+
 $(function () {
+
     $("[rel=tooltip]").tooltip();
 
     $("#top-link").click(function () {
       $("body").animate({ scrollTop: $(".header").offset().top }, "slow");
     });
+
+    $("#years").html(getAge($("#years").data("birthdate")));
 });
